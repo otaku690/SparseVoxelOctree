b688d0e649d30354c819b62fec60df60efcebe3b
Cheng-Tso Lin <otaku690@gmail.com>
2013-12-07 21:58:35 -0500

    Add octree node initialization compute shader

---
+++ WIN/SVO/shader/nodeInit.com.glsl
@@ -0,0 +1,22 @@
+// Sparse Voxel Octree and Voxel Cone Tracing
+// 
+// University of Pennsylvania CIS565 final project
+// copyright (c) 2013 Cheng-Tso Lin 
+ 
+# version 430
+layout (local_size_x = 64, local_size_y = 1, local_size_z = 1 ) in;
+
+uniform int u_allocStart;
+uniform int u_num;
+
+uniform layout( binding=1, r32ui ) uimageBuffer u_octreeBuf;
+
+void main()
+{
+    uint offset;
+    uint thxId = gl_GlobalInvocationID.x;
+	if( thxId >= u_num )
+	    return;
+
+	imageStore( u_octreeBuf, u_allocStart + int(thxId), uvec4(0,0,0,0) );
+}
